a0fbdd79636488130f7d1e6cdcf22b6a9c6cd67f
jessicaloureiro <jessica.loureiro-simoes@wdr.de>
2024-02-21 10:10:32 +0100

    first draft ingest service for a single item

---
+++ microservices/ingestservice/config.py
@@ -0,0 +1,7 @@
+#config.py
+
+class Settings:
+    PROJECT_NAME:str = "Ingest Service"
+    PROJECT_VERSION: str = "1.0.0"
+
+settings = Settings()
---
+++ microservices/ingestservice/main.py
@@ -0,0 +1,70 @@
+#main.py
+
+# Start with uvicorn main:app --reload
+
+from fastapi import FastAPI
+from microservices.ingestservice.config import settings
+from microservices.ingestservice.dto.recoexplorer_item import RecoExplorerItem
+from microservices.ingestservice.data_mapper import DataMapper
+from microservices.ingestservice.dto.media_data import MediaData
+from microservices.ingestservice.dto.sophora_data import SophoraData
+import os
+import logging
+from opensearchpy import OpenSearch, RequestsHttpConnection, helpers
+from envyaml import EnvYAML
+import json
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+
+path = 'microservices/ingestservice/' # THIS NEEDS TO BE CHANGED
+
+@app.post("/map-data", response_model=RecoExplorerItem)
+def map_data(data: MediaData) -> RecoExplorerItem: # input = entity
+    data_mapper = DataMapper()
+    mapped_data = data_mapper.map_data(data)
+    return mapped_data
+
+@app.post("/ingest-data") #, response_model=RecoExplorerItem)
+def ingest_data(client, data: SophoraData) -> RecoExplorerItem: # input = entity
+    mapped_data = map_data(data).model_dump()
+
+
+    # get values from config
+    config_full_path = path + '/config_' + client + '.yaml'
+    config = EnvYAML(config_full_path)
+
+    # MOVE THIS TO SEPERATE FILE + pass config
+    target_idx_name = config['opensearch']['index']
+
+    host = config['opensearch']['host']
+    auth = (config['opensearch']['user'],
+            config['opensearch']['pass'])
+    port = config['opensearch']['port']
+
+    logger.info('Host: ' + host)
+
+
+    # initialize OSS client
+    oss_client = OpenSearch(
+        hosts=[{"host": host, "port": port}],
+        http_auth=auth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection,
+        timeout=600,
+    )
+
+    # add document to index
+    response = oss_client.index(
+        index=target_idx_name,
+        body=mapped_data,
+        id=f"{mapped_data['id']}",
+        refresh=True
+    )
+
+    # logger.info('Response: ' + response)
+
+    return mapped_data
