4f3d9796da8889e6cc6a3a1656dafeb640affef0
jessicaloureiro <jessica.loureiro-simoes@wdr.de>
2024-02-22 17:17:55 +0100

    added dtos and config

---
+++ microservices/ingestservice/dto/recoexplorer_item.py
@@ -0,0 +1,44 @@
+from typing import Optional
+from pydantic import BaseModel
+from pydantic import fields
+from datetime import datetime
+
+
+class RecoExplorerItem(BaseModel):
+    externalid: str
+    id: Optional[str] = ''
+    title: str
+    description: str
+    longDescription: str
+    availableFrom: datetime
+    availableTo: datetime
+    duration: Optional[int] = None
+    thematicCategories: list
+    thematicCategoriesIds: Optional[list] = []
+    thematicCategoriesTitle: Optional[list] = []
+    genreCategory: str
+    genreCategoryId: Optional[str] = ''
+    subgenreCategories: list
+    subgenreCategoriesIds: Optional[list] = []
+    subgenreCategoriesTitle: Optional[list] = []
+    teaserimage: str
+    geoAvailability: Optional[str] = ''
+    embedText: Optional[str] = ''
+    embedTextHash: Optional[str] = ''
+    episodeNumber: Optional[str] = ''
+    hasAudioDescription: Optional[bool] = None
+    hasDefaultVersion: Optional[bool] = None
+    hasSignLanguage: Optional[bool] = None
+    hasSubtitles: Optional[bool] = None
+    isChildContent: Optional[bool] = None
+    isOnlineOnly: Optional[bool] = None
+    isOriginalLanguage: Optional[bool] = None
+    producer: Optional[str] = ''
+    publisherId: Optional[str] = ''
+    seasonNumber: Optional[str] = ''
+    sections: Optional[str] = ''
+    showCrid: Optional[str] = ''
+    showId: Optional[str] = ''
+    showTitel: Optional[str] = ''
+    showType: Optional[str] = ''
+    startDate: Optional[datetime] = None
