bbf0eeec548f22a3cfdea0c7d7f372b601aff53e
jessicaloureiro <jessica.loureiro-simoes@wdr.de>
2024-02-28 14:50:44 +0100

    refactor ingest service

--- microservices/ingestservice/src/main.py
+++ microservices/ingestservice/src/main.py
@@ -24,7 +24,7 @@ oss_doc_generator = OssAccessor(config)
 
 def request(data, url):
     # TODO: remove timeout when search service is implemented
-    response = httpx.post(url, json=data, timeout = None)
+    response = httpx.post(url, json=data, timeout=None)
     return response.json()
 
 
@@ -40,7 +40,7 @@ def health_check():
 def ingest_item(data: dict):
     mapped_data = data_preprocessor.preprocess_data(data)
     # add data to index
-    search_response = oss_doc_generator.create_oss_doc(mapped_data)
+    search_response = request(mapped_data, f"{BASE_URL_SEARCH}/create-single-document")
 
     return search_response
 
@@ -48,14 +48,15 @@ def ingest_item(data: dict):
 @router.post("/ingest-multiple-items")
 def bulk_ingest(bucket):
     item_dict = {}
-    for fname in glob(bucket+"*.json"):
-        with open(fname, 'r') as f:
+    for fname in glob(bucket + "*.json"):
+        with open(fname, "r") as f:
             data = json.load(f)
             mapped_data = data_preprocessor.preprocess_data(data)
-            item_dict[mapped_data['id']] = mapped_data
-    search_response = oss_doc_generator.bulk_ingest(item_dict)
+            item_dict[mapped_data["id"]] = mapped_data
+    search_response = request(item_dict, f"{BASE_URL_SEARCH}/create-multiple-documents")
 
     return search_response
+
 
 @router.delete("/delete-data/{id}")
 def delete_document(document_id):
@@ -65,3 +66,4 @@ def delete_document(document_id):
 
 app = FastAPI(title="Ingest Service")
 app.include_router(router, prefix=ROUTER_PREFIX)
+
--- microservices/ingestservice/src/oss_accessor.py
+++ microservices/ingestservice/src/oss_accessor.py
@@ -7,19 +7,20 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
 class OssAccessor:
+    def __init__(self):
+        pass
 
-    def __init__(self, config):
-        self.target_idx_name = config['opensearch']['index']
+    def create_oss_doc(self, config, mapped_data):
+        target_idx_name = config["opensearch"]["index"]
 
-        host = config['opensearch']['host']
-        auth = (config['opensearch']['user'],
-                config['opensearch']['pass'])
-        port = config['opensearch']['port']
+        host = config["opensearch"]["host"]
+        auth = (config["opensearch"]["user"], config["opensearch"]["pass"])
+        port = config["opensearch"]["port"]
 
-        logger.info('Host: ' + host)
+        logger.info("Host: " + host)
 
         # initialize OSS client
-        self.oss_client = OpenSearch(
+        oss_client = OpenSearch(
             hosts=[{"host": host, "port": port}],
             http_auth=auth,
             use_ssl=True,
@@ -28,42 +29,12 @@ class OssAccessor:
             timeout=600,
         )
 
-    def create_oss_doc(self, data):
         # add document to index
-        response = self.oss_client.update(
-            index=self.target_idx_name,
-            body={"doc": data, "doc_as_upsert": True},
-            id=f"{data['id']}",
-            refresh=True
+        response = oss_client.index(
+            index=target_idx_name,
+            body=mapped_data,
+            id=f"{mapped_data['id']}",
+            refresh=True,
         )
 
-        logger.info('Response: ' + json.dumps(response, indent=4, default=str))
-
-        return response
-
-    def delete_oss_doc(self, id):
-        response = self.oss_client.delete(
-            index=self.target_idx_name,
-            id=id
-        )
-        return response
-
-    def bulk_ingest(self, jsonlst):
-        for success, info in helpers.parallel_bulk(
-                    client=self.oss_client,
-                    index=self.target_idx_name,
-                    actions=self.doc_generator(jsonlst)
-                ):
-            if not success:
-                print('A document failed:', info)
-
-    def doc_generator(self, jsonlst): # TODO: review this
-        for idx in jsonlst:
-            item=jsonlst[idx]
-
-            yield {
-                "_op_type": "update",
-                "_index": self.target_idx_name,
-                "_id": f"{item['id']}",
-                "_source": {"doc": item, "doc_as_upsert": True}
-            }
+        logger.info("Response: " + json.dumps(response, indent=4, default=str))
