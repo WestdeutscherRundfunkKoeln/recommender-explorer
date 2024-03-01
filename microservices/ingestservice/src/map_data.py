8f61649fce4eb22085408ed40f2378ba11961f04
jessicaloureiro <jessica.loureiro-simoes@wdr.de>
2024-03-01 15:15:06 +0100

    refactor config

--- microservices/ingestservice/src/map_data.py
+++ microservices/ingestservice/src/map_data.py
@@ -12,12 +12,12 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 class DataMapper:
 
     def __init__(self, config):
-        self.mapping = config['opensearch']['mapping']
+        mapping_definition_file = config['mapping_definition_file']
+        mapping_file = EnvYAML(mapping_definition_file)
+        self.mapping = mapping_file['mapping']
 
     def map_data(self, data) -> RecoExplorerItem: # input = entity
         mapped_data = pyjq.one(self.mapping, data)
-
         logger.info('Mapped data: ' + json.dumps(mapped_data, indent=4, default=str))
-
         response = RecoExplorerItem.model_validate(mapped_data)
         return response
