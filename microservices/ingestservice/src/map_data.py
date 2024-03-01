dba43c1b61d3f8ee972382d39e184fe6549cb71c
jessicaloureiro <jessica.loureiro-simoes@wdr.de>
2024-03-01 13:50:36 +0100

    push latest changes

--- microservices/ingestservice/src/map_data.py
+++ microservices/ingestservice/src/map_data.py
@@ -1,31 +1,23 @@
 from envyaml import EnvYAML
-from microservices.ingestservice.dto.media_data import MediaData
-from microservices.ingestservice.dto.sophora_data import SophoraData
-from microservices.ingestservice.dto.recoexplorer_item import RecoExplorerItem
+from dto.recoexplorer_item import RecoExplorerItem
 import logging
 import json
+import pyjq
+# import jq
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
+
 class DataMapper:
 
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        self.mapping = config['opensearch']['mapping']
 
-    def map_data(self, config, data: SophoraData) -> RecoExplorerItem: # input = entity
-        mapped_data = {}
-        for item in data:
-            if item[0] in config['opensearch']['mapping']:
-                mapped_key = config['opensearch']['mapping'][item[0]]
-                if item[0] in config['opensearch']['restructuring']:
-                    dict_key = config['opensearch']['restructuring'][item[0]]
-                    mapped_data[mapped_key] = item[1][dict_key]
-                else:
-                    mapped_data[mapped_key] = item[1]
-            else:
-                mapped_data[item[0]] = item[1]
+    def map_data(self, data) -> RecoExplorerItem: # input = entity
+        mapped_data = pyjq.one(self.mapping, data)
 
         logger.info('Mapped data: ' + json.dumps(mapped_data, indent=4, default=str))
+
         response = RecoExplorerItem.model_validate(mapped_data)
         return response
