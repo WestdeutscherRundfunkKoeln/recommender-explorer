76abe4d30e2e82ef641817090568e7f77f24ec9b
nwittenberg <139441972+nwittenberg@users.noreply.github.com>
2024-04-02 11:41:42 +0200

    SNTW-975: Data Indexing Microservices - initial setup (#23)
    * first draft ingest service for a single item
    
    * added dtos and config
    
    * refactor ingest service
    
    * Add Dockerfile
    
    * push latest changes
    
    * refactor config
    
    * refactor
    
    * first embedding service
    
    * added search service
    
    * added embedding service
    
    * adjust config pass & dockerfile
    
    * add health check route
    
    * adjust field extraction for mapping def
    
    * added bulk ingest and refactored embedding service
    
    * rfactoring of ingest and search service
    
    * adjusted init in preprocess_data
    
    * temporary fix of ingest service for first deployment
    
    * added oss accessor to dockerfile
    
    ---------
    
    Co-authored-by: jessicaloureiro <jessica.loureiro-simoes@wdr.de>
    Co-authored-by: Sebastian Bothe <sebastian.bothe@wdr.de>
    Co-authored-by: jessicaloureiro <jessicaloureiro1996@hotmail.fr>

---
+++ microservices/ingestservice/dto/recoexplorer_item.py
@@ -0,0 +1,54 @@
+from typing import Optional
+from pydantic import BaseModel, Extra, ConfigDict, field_serializer
+from datetime import datetime
+
+
+class RecoExplorerItem(BaseModel, extra=Extra.allow):
+    externalid: str
+    id: str
+    title: str
+    description: str
+    longDescription: str
+    availableFrom: datetime
+    availableTo: datetime
+    duration: Optional[int] = -1
+    thematicCategories: list
+    thematicCategoriesIds: Optional[list] = []
+    thematicCategoriesTitle: Optional[list] = []
+    genreCategory: str
+    genreCategoryId: Optional[str] = ''
+    subgenreCategories: list
+    subgenreCategoriesIds: Optional[list] = []
+    subgenreCategoriesTitle: Optional[list] = []
+    teaserimage: str
+    geoAvailability: Optional[str] = ''
+    embedText: Optional[str] = ''
+    embedTextHash: Optional[str] = ''
+    episodeNumber: Optional[str] = ''
+    hasAudioDescription: Optional[bool] = False
+    hasDefaultVersion: Optional[bool] = False
+    hasSignLanguage: Optional[bool] = False
+    hasSubtitles: Optional[bool] = False
+    isChildContent: Optional[bool] = False
+    isOnlineOnly: Optional[bool] = False
+    isOriginalLanguage: Optional[bool] = False
+    producer: Optional[str] = ''
+    publisherId: Optional[str] = ''
+    seasonNumber: Optional[str] = ''
+    sections: Optional[str] = ''
+    showCrid: Optional[str] = ''
+    showId: Optional[str] = ''
+    showTitel: Optional[str] = '' # showTitle?
+    showType: Optional[str] = ''
+
+    @field_serializer('availableFrom')
+    def serialize_dt(self, dt: datetime, _info):
+        return dt.timestamp()
+
+    @field_serializer('availableTo')
+    def serialize_dt(self, dt: datetime, _info):
+        return dt.timestamp()
+
+    @property
+    def extra_fields(self) -> set[str]: # accept extra fields from client which are not defined in class
+        return set(self.__dict__) - set(self.__fields__)
---
+++ microservices/searchservice/src/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import ValidationError
+from src.oss_accessor import OssAccessor
+from envyaml import EnvYAML
+import os
+
+app = FastAPI(title="Ingest Service")
+# get values from config
+full_path = os.environ.get("CONFIG_FILE", default='config.yaml')
+config = EnvYAML(full_path)
+oss_doc_generator = OssAccessor(config)
+
+
+@app.post("/create-single-document")
+def create_document(data: dict):
+    # add data to index
+    response = oss_doc_generator.create_oss_doc(data)
+    return response
+
+@app.delete("/delete-data/{id}")
+def delete_document(document_id):
+    response = oss_doc_generator.delete_oss_doc(document_id)
+    return response
+
+@app.post("/create-multiple-documents")
+def bulk_create_document(data: dict):
+    # add data to index
+    response = oss_doc_generator.bulk_ingest(data)
+    return response
+
+# TODO: search query for nearest neighbors
