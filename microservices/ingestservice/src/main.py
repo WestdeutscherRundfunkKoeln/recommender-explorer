9288a4a0fb2405e228d32648e0f429fdc8b1ecbc
nwittenberg <natalja.wittenberg@ivz.de>
2024-04-03 10:48:46 +0200

    add api prefix handler ingest ms & small refactoring

--- microservices/ingestservice/src/main.py
+++ microservices/ingestservice/src/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import ValidationError
+from fastapi import FastAPI, APIRouter
 from src.preprocess_data import DataPreprocessor
 from src.oss_accessor import OssAccessor
 from envyaml import EnvYAML
@@ -8,35 +7,43 @@ import httpx
 from glob import glob
 import json
 
-app = FastAPI(title="Ingest Service")
-# get values from config
-full_path = os.environ.get("CONFIG_FILE", default='config.yaml')
-config = EnvYAML(full_path)
+NAMESPACE = "ingest"
+
+CONFIG_PATH = os.environ.get("CONFIG_FILE", default="config.yaml")
+BASE_URL_SEARCH = os.environ.get("BASE_URL_SEARCH", default="")
+API_PREFIX = os.environ.get("API_PREFIX", default="")
+ROUTER_PREFIX = os.path.join(API_PREFIX, NAMESPACE) if API_PREFIX else ""
+
+config = EnvYAML(CONFIG_PATH)
 data_preprocessor = DataPreprocessor(config)
 oss_doc_generator = OssAccessor(config)
 
-# URL_SEARCH_SINGLE = os.environ.get("URL_SEARCH_SINGLE")
-# URL_SEARCH_BULK = os.environ.get("URL_SEARCH_BULK")
 
 def request(data, url):
-    response = httpx.post(url, json=data, timeout = None) # TODO: remove timeout when search service is implemented
+    # TODO: remove timeout when search service is implemented
+    response = httpx.post(url, json=data, timeout = None)
     return response.json()
 
-@app.get("/health-check")
+
+router = APIRouter()
+
+
+@router.get("/health-check")
 def health_check():
     return {"status": "OK"}
 
-@app.post("/ingest-single-item")
+
+@router.post("/ingest-single-item")
 def ingest_item(data: dict):
     mapped_data = data_preprocessor.preprocess_data(data)
     # add data to index
-    # search_response = request(mapped_data, URL_SEARCH_SINGLE)
+    # search_response = request(mapped_data,f"{BASE_URL_SEARCH}/create-single-document")
     response = oss_doc_generator.create_oss_doc(mapped_data)
 
     return response
 
 
-@app.post("/ingest-multiple-items")
+@router.post("/ingest-multiple-items")
 def bulk_ingest(bucket):
     item_dict = {}
     for fname in glob(bucket+"*.json"):
@@ -44,12 +51,17 @@ def bulk_ingest(bucket):
             data = json.load(f)
             mapped_data = data_preprocessor.preprocess_data(data)
             item_dict[mapped_data['id']] = mapped_data
-    # search_response = request(item_dict, URL_SEARCH_BULK)
+    # search_response = request(item_dict, f"{BASE_URL_SEARCH}/create-multiple-documents")
     response = oss_doc_generator.bulk_ingest(item_dict)
 
     return response
 
-@app.delete("/delete-data/{id}")
+
+@router.delete("/delete-data/{id}")
 def delete_document(document_id):
     response = oss_doc_generator.delete_oss_doc(document_id)
     return response
+
+
+app = FastAPI(title="Ingest Service")
+app.include_router(router, prefix=ROUTER_PREFIX)
