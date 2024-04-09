398a97e0e6039b123d0eb01284610a25fd4e92de
Michael Gruschke <michael.gruschke@pub.tech>
2024-04-09 15:46:36 +0200

    adjust config pass & dockerfile

---
+++ microservices/ingestservice/src/map_data.py
@@ -0,0 +1,21 @@
+from envyaml import EnvYAML
+from dto.recoexplorer_item import RecoExplorerItem
+import logging
+import json
+import pyjq
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+class DataMapper:
+
+    def __init__(self, config):
+        mapping_definition = config['mapping_definition']
+        self.mapping = mapping_definition['mapping']
+
+    def map_data(self, data) -> RecoExplorerItem: # input = entity
+        mapped_data = pyjq.one(self.mapping, data)
+        logger.info('Mapped data: ' + json.dumps(mapped_data, indent=4, default=str))
+        response = RecoExplorerItem.model_validate(mapped_data)
+        return response
