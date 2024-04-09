74d13d71572eda620f8ff6b89b78a6cc18b2ad97
Michael Gruschke <michael.gruschke@pub.tech>
2024-04-09 15:27:50 +0200

    refactor

--- microservices/ingestservice/src/map_data.py
+++ microservices/ingestservice/src/map_data.py
@@ -3,7 +3,6 @@ from dto.recoexplorer_item import RecoExplorerItem
 import logging
 import json
 import pyjq
-# import jq
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(message)s")
