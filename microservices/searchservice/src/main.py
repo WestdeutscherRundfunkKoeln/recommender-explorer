e3bb9fcc7b6854cbcdfaea5f0d49389c83fff09c
Michael Gruschke <michael.gruschke@pub.tech>
2024-04-22 08:55:55 +0200

    feat: change delete endpoint id as query param

--- microservices/searchservice/src/main.py
+++ microservices/searchservice/src/main.py
@@ -38,8 +38,8 @@ def bulk_create_document(data: dict):
     return response
 
 
-@router.delete("/delete-data/{document_id}")
-def delete_document(document_id):
+@router.delete("/delete-data")
+def delete_document(document_id: str):
     response = oss_doc_generator.delete_oss_doc(document_id)
     return response
 
