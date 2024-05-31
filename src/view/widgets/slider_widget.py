0cb7b5279e3b3af4493242b4bdf1eea6461c2380
Michael Gruschke <124264411+grusc@users.noreply.github.com>
2024-05-31 10:45:19 +0200

    refactor: introduce import alias

--- src/view/widgets/slider_widget.py
+++ src/view/widgets/slider_widget.py
@@ -4,19 +4,19 @@ from bokeh.models.formatters import PrintfTickFormatter
 import panel as pn
 from view.widgets.widget import UIWidget
 
-from view import ui_constants
+from view import ui_constants as c
 
 
 class SliderWidget(UIWidget):
     def create(self, config: dict[str, Any]) -> pn.widgets.FloatSlider:
         slider = pn.widgets.FloatSlider(
-            name=config.get(ui_constants.SLIDER_LABEL_KEY, "Wert"),
+            name=config.get(c.SLIDER_LABEL_KEY, "Wert"),
             format=PrintfTickFormatter(
-                format=f"%.2f {config.get(ui_constants.SLIDER_UNIT_KEY, '')}"
+                format=f"%.2f {config.get(c.SLIDER_UNIT_KEY, '')}"
             ),
-            start=config.get(ui_constants.SLIDER_START_KEY, 0),
-            end=config.get(ui_constants.SLIDER_END_KEY, 1),
-            step=config.get(ui_constants.SLIDER_STEP_KEY, 0.01),
-            value=config.get(ui_constants.SLIDER_START_KEY, 0),
+            start=config.get(c.SLIDER_START_KEY, 0),
+            end=config.get(c.SLIDER_END_KEY, 1),
+            step=config.get(c.SLIDER_STEP_KEY, 0.01),
+            value=config.get(c.SLIDER_START_KEY, 0),
         )
         return slider
