4e748fe3ac44fc501a10b782f50c13cf4187094e
Michael Gruschke <michael.gruschke@pub.tech>
2024-06-05 12:37:01 +0200

    feat: add duration filter wiring

--- src/model/paservice/base_data_accessor_pa_service.py
+++ src/model/paservice/base_data_accessor_pa_service.py
@@ -18,34 +18,48 @@ class BaseDataAccessorPaService(BaseDataAccessor):
     def __init__(self, config):
         self.not_implemented_error_message = "This method is not implemented yet"
         self.config = config
-        self.pa_service_config = self.config.get('pa_service', None)
+        self.pa_service_config = self.config.get("pa_service", None)
         if self.pa_service_config:
-            self.host = self.pa_service_config.get('host', None)
-            self.auth_header = self.pa_service_config.get('auth_header', None)
-            self.auth_header_value = self.pa_service_config.get('auth_header_value', None)
-            self.http_proxy = self.pa_service_config.get('http_proxy', None)
-            self.https_proxy = self.pa_service_config.get('https_proxy', None)
-            self.endpoint = self.pa_service_config.get('endpoint', None)
-            self.field_mapping = self.pa_service_config.get('field_mapping', None)
+            self.host = self.pa_service_config.get("host", None)
+            self.auth_header = self.pa_service_config.get("auth_header", None)
+            self.auth_header_value = self.pa_service_config.get(
+                "auth_header_value", None
+            )
+            self.http_proxy = self.pa_service_config.get("http_proxy", None)
+            self.https_proxy = self.pa_service_config.get("https_proxy", None)
+            self.endpoint = self.pa_service_config.get("endpoint", None)
+            self.field_mapping = self.pa_service_config.get("field_mapping", None)
 
         if not self.pa_service_config:
-            raise ConfigError("Could not get valid Configuration for Service from config yaml. BaseDataAccessorPaService needs correctly configured Service, "
-                              "expect Configuration in Key: pa_service")
+            raise ConfigError(
+                "Could not get valid Configuration for Service from config yaml. BaseDataAccessorPaService needs correctly configured Service, "
+                "expect Configuration in Key: pa_service"
+            )
         elif not self.host:
-            raise ConfigError("Could not get valid Configuration for Service from config yaml. BaseDataAccessorPaService needs correctly configured Service, "
-                              "expect Configuration in Key: pa_service.host")
+            raise ConfigError(
+                "Could not get valid Configuration for Service from config yaml. BaseDataAccessorPaService needs correctly configured Service, "
+                "expect Configuration in Key: pa_service.host"
+            )
         elif not self.endpoint:
-            raise ConfigError("Could not get valid Configuration for Service from config yaml. BaseDataAccessorPaService needs correctly configured Service, "
-                              "expect Configuration in Key: pa_service.endpoint")
+            raise ConfigError(
+                "Could not get valid Configuration for Service from config yaml. BaseDataAccessorPaService needs correctly configured Service, "
+                "expect Configuration in Key: pa_service.endpoint"
+            )
         elif not self.field_mapping:
-            raise ConfigError("Could not get valid Configuration for Service from config yaml. BaseDataAccessorPaService needs correctly configured Service, "
-                              "expect Configuration in Key: pa_service.field_mapping")
+            raise ConfigError(
+                "Could not get valid Configuration for Service from config yaml. BaseDataAccessorPaService needs correctly configured Service, "
+                "expect Configuration in Key: pa_service.field_mapping"
+            )
         elif not self.auth_header:
-            raise ConfigError("Could not get valid Configuration for Service from config yaml. BaseDataAccessorPaService needs correctly configured Service, "
-                              "expect Configuration in Key: pa_service.auth_header")
+            raise ConfigError(
+                "Could not get valid Configuration for Service from config yaml. BaseDataAccessorPaService needs correctly configured Service, "
+                "expect Configuration in Key: pa_service.auth_header"
+            )
         elif not self.auth_header_value:
-            raise ConfigError("Could not get valid Configuration for Service from config yaml. BaseDataAccessorPaService needs correctly configured Service, "
-                              "expect Configuration in Key: pa_service.auth_header_value")
+            raise ConfigError(
+                "Could not get valid Configuration for Service from config yaml. BaseDataAccessorPaService needs correctly configured Service, "
+                "expect Configuration in Key: pa_service.auth_header_value"
+            )
 
     def get_items_by_ids(self, ids):
         raise NotImplementedError(self.not_implemented_error_message)
@@ -78,35 +92,30 @@ class BaseDataAccessorPaService(BaseDataAccessor):
         """
         try:
             if self.host and self.endpoint:
-                url = self.host + '/' + self.endpoint
+                url = self.host + "/" + self.endpoint
 
-                request_body = {
-                    "referenceId": external_id,
-                    "reco": "true"
-                }
+                request_body = {"referenceId": external_id, "reco": "true"}
+                if "relativerangefilter_duration" in filter:
+                    request_body["maxDurationFactor"] = filter[
+                        "relativerangefilter_duration"
+                    ]
 
-                request_params = {
-                    'url': url,
-                    'json': request_body
-                }
+                request_params = {"url": url, "json": request_body}
 
                 headers = None
                 if self.auth_header and self.auth_header_value:
                     headers = {
-                        'Content-Type': 'application/json',
-                        'accept': '*/*',
-                        self.auth_header: self.auth_header_value
+                        "Content-Type": "application/json",
+                        "accept": "*/*",
+                        self.auth_header: self.auth_header_value,
                     }
 
                 if headers:
-                    request_params['headers'] = headers
+                    request_params["headers"] = headers
 
                 proxies = None
                 if self.http_proxy and self.https_proxy:
-                    proxies = {
-                        'http://': self.http_proxy,
-                        'https://': self.https_proxy
-                    }
+                    proxies = {"http://": self.http_proxy, "https://": self.https_proxy}
 
                 if proxies:
                     client = httpx.Client(proxies=proxies)
@@ -120,12 +129,27 @@ class BaseDataAccessorPaService(BaseDataAccessor):
                 else:
                     logging.error("Request Error:", response.status_code, response.text)
                     raise EndpointError(
-                        'Could not get result from Endpoint: ' + url + 'with request parameters: ' + json.dumps(request_params, indent=4) + '. Status Code: ' + response.status_code)
+                        "Could not get result from Endpoint: "
+                        + url
+                        + "with request parameters: "
+                        + json.dumps(request_params, indent=4)
+                        + ". Status Code: "
+                        + response.status_code
+                    )
         except Exception as e:
             logging.error(e)
-            raise EndpointError("Couldn't get a valid response from endpoint [" + self.host + '/' + self.endpoint + ']', {})
+            raise EndpointError(
+                "Couldn't get a valid response from endpoint ["
+                + self.host
+                + "/"
+                + self.endpoint
+                + "]",
+                {},
+            )
 
-    def __get_items_from_response(self, item_dto: ItemDto, response, provenance=constants.ITEM_PROVENANCE_C2C) -> tuple[list, int]:
+    def __get_items_from_response(
+        self, item_dto: ItemDto, response, provenance=constants.ITEM_PROVENANCE_C2C
+    ) -> tuple[list, int]:
         """
         Gets the resulting items from the pa service response in json
         Gets total items count from search response and iterates over result items and map
@@ -136,7 +160,7 @@ class BaseDataAccessorPaService(BaseDataAccessor):
         :param provenance:
         :return: List of item dtos, total items count
         """
-        items = response['items']
+        items = response["items"]
         total_items = len(items)
 
         if total_items < 1 or not len(items):
@@ -144,12 +168,12 @@ class BaseDataAccessorPaService(BaseDataAccessor):
         item_dtos = []
         for index, pa_service_hit in enumerate(items):
             new_item_dto = copy.copy(item_dto)
-            item_hit = pa_service_hit['item']
+            item_hit = pa_service_hit["item"]
             new_item_dto = update_from_props(new_item_dto, item_hit, self.field_mapping)
-            new_item_dto.__setattr__('score', pa_service_hit.get('score', 0))
+            new_item_dto.__setattr__("score", pa_service_hit.get("score", 0))
             if index == 0:
-                new_item_dto.__setattr__('_position', 'start')
+                new_item_dto.__setattr__("_position", "start")
             else:
-                new_item_dto.__setattr__('_position', 'reco')
+                new_item_dto.__setattr__("_position", "reco")
             item_dtos.append(new_item_dto)
         return item_dtos, total_items
