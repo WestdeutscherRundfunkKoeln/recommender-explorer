2548e87b9a1bf77c002151fd8155084510e36947
Michael Gruschke <michael.gruschke@pub.tech>
2024-06-12 10:51:12 +0200

    Revert "feat: implement display_name in multiselect"
    This reverts commit 858bc67e0fccf6fd58646d276151de13a8683644.

--- src/view/widgets/multi_select_widget.py
+++ src/view/widgets/multi_select_widget.py
@@ -58,17 +58,17 @@ class MultiSelectionWidget(UIWidget):
         if not options:
             return
 
-        multi_select_name = config.get(c.MULTI_SELECT_DISPLAY_NAME_KEY, "")
+        multi_select_label = config.get(c.MULTI_SELECT_LABEL_KEY, "")
 
         multi_select_widget = pn.widgets.MultiSelect(
             options=options,
             value=default,
             size=min(len(options), 5),
-            name=multi_select_name,
+            name=multi_select_label,
         )
 
         multi_select_widget.params = {
-            "label": config.get(c.MULTI_SELECT_LABEL_KEY, ""),
+            "label": multi_select_label,
             "reset_to": default,
         }
         return multi_select_widget
