2b3a8c7c380762d5e0dee7ca57b13997ecc7ac7c
Michael Gruschke <michael.gruschke@pub.tech>
2024-06-26 10:28:35 +0200

    refactor: custom request models and streamline implementation

--- microservices/embeddingservice/src/main.py
+++ microservices/embeddingservice/src/main.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI, HTTPException, APIRouter
-from pydantic import ValidationError
+from fastapi import FastAPI, APIRouter
 from src.embed_text import EmbedText
-from dto.embed_data import EmbedData
+from dto.embed_data import AddEmbeddingToDocRequest, EmbeddingRequest
 from envyaml import EnvYAML
 import os
 
@@ -9,7 +8,7 @@ NAMESPACE = "embedding"
 
 CONFIG_PATH = os.environ.get("CONFIG_FILE", default="config.yaml")
 
-#TODO: Remove direct environment var access, use config file instead!!
+# TODO: Remove direct environment var access, use config file instead!!
 API_PREFIX = os.environ.get("API_PREFIX", default="")
 ROUTER_PREFIX = os.path.join(API_PREFIX, NAMESPACE) if API_PREFIX else ""
 
@@ -21,37 +20,22 @@ router = APIRouter()
 
 @router.get("/health-check")
 def health_check():
-    #TODO: Add a more sensful check for service health!
+    # TODO: Add a more sensful check for service health!
     # For instance check, if embedder is working
     return {"status": "OK"}
 
-@router.post("/embedding")
-def get_embedding(data: EmbedData):
-    text_to_embed = data.embedText
-    models_to_use = data.models
-    try:
-        result = text_embedder.embed_text(text_to_embed, models_to_use)
-    except ValidationError as exc:
-        error_message = repr(exc.errors()[0]['type'])
-        raise HTTPException(status_code=422, detail=error_message)
 
-    return result
+@router.post("/embedding")
+def get_embedding(data: EmbeddingRequest):
+    return text_embedder.embed_text(data.embedText, data.models)
+
 
 @router.post("/add-embedding-to-doc")
-def add_embedding_to_document(data: EmbedData):
-    id = data.id
-    text_to_embed = data.embedText
-
-    try:
-        result = text_embedder.embed_text(text_to_embed, [])
-        text_embedder.add_embedding_to_document(id, result)
-    except ValidationError as exc:
-        error_message = repr(exc.errors()[0]["type"])
-        raise HTTPException(status_code=422, detail=error_message)  # TODO: Return here
-
+def add_embedding_to_document(data: AddEmbeddingToDocRequest):
+    result = text_embedder.embed_text(data.embedText, [])
+    text_embedder.add_embedding_to_document(data.id, result)
     return result
 
 
 app = FastAPI(title="Embedding Service")
 app.include_router(router, prefix=ROUTER_PREFIX)
-
