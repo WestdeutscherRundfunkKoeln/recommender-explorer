06dc50f169abb7883a4da76921c196d757057682
Michael Gruschke <michael.gruschke@pub.tech>
2024-06-26 13:07:44 +0200

    feat: change paservice_cards to look like WDR

--- src/view/cards/paservice_cards/pa_service_content_card.py
+++ src/view/cards/paservice_cards/pa_service_content_card.py
@@ -6,21 +6,32 @@ logger = logging.getLogger(__name__)
 
 
 class PaServiceContentCard:
+    type_icon = {
+        "beitrag": "📰",
+        "audio": "🔊",
+        "video": "🎥",
+    }
+
+    domain_mapping = {"wdr.de": "https://www1.wdr.de"}
 
     def __init__(self, config):
         self.config = config
 
     def draw(self, content_dto: PaServiceContentItemDto, card):
-
         base_card_objects = [
             pn.pane.Markdown(f"""
                        #### {content_dto.title}
-                       **Datentyp:** {content_dto.contentType} 
+                       **Datentyp:** {self.type_icon.get(content_dto.contentType, content_dto.contentType)} 
+                       **Datum:** {content_dto.availableFrom}
                        **Strukturpfad:** {content_dto.structureNodePath}
-                       **External ID:** {content_dto.externalId} 
-                       **Sophora ID:** {content_dto.sophoraId}
+                       **External ID:** [{content_dto.externalId}]({self.config["reco_explorer_url_base"]}?crid={content_dto.externalId}) 
+                       **Sophora ID:** [{content_dto.sophoraId}](https://{content_dto.domain}{content_dto.structureNodePath}/{content_dto.sophoraId}.html)
                 """),
-            content_dto.description
+            pn.pane.Markdown(f"""
+            ***
+            ##### {content_dto.title}
+            {content_dto.description[:2000]}...
+            """),
         ]
 
         card.objects = card.objects + base_card_objects
