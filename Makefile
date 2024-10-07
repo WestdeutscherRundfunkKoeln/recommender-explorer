4b5089f02fe5c965891243c0407a92425cc0fd7c
Michael Gruschke <124264411+grusc@users.noreply.github.com>
2024-10-07 11:33:30 +0200

    SNTWREC-245: NNSeeker gibt sophoraIds zurück (#109)
    * fix: let nn_seeker_opensearch return externalids
    
    * feat: use opensearch internal id for nnseeker
    
    * fix: tests
    
    * chore: add testing config

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
 
 .PHONY: run_tests_integration_microservices
 run_tests_integration_microservices:
-	docker-compose --profile mock_gcs up --build -d --wait
+	docker compose up --build -d --wait
 	python -m pytest test/integration/microservices || exit 0
-	docker-compose --profile mock_gcs kill
-	docker-compose --profile mock_gcs rm -f
-	docker image prune -f
+	docker compose  kill
+	docker compose  rm -f
 
+
+.PHONY: run_tests_integration_controller
+run_tests_integration_controller:
+	docker compose up --build -d --wait
+	python -m pytest --config=config/config_testing.yaml test/integration/test_controller_integrations.py || exit 0
+	docker compose  kill
+	docker compose  rm -f
 
--- test/conftest.py
+++ test/conftest.py
@@ -1,53 +1,77 @@
 import pytest
 import logging
-import constants
+import src.constants as constants
 from envyaml import EnvYAML
-from controller.reco_controller import RecommendationController
-from test.test_util import mock_user_component, mock_model_component, mock_date_component
+from src.controller.reco_controller import RecommendationController
+from test.test_util import (
+    mock_user_component,
+    mock_model_component,
+    mock_date_component,
+)
 
 logger = logging.getLogger(__name__)
 
+
 def pytest_addoption(parser):
     parser.addoption("--config", action="store", default="./config_default.yaml")
+
 
 @pytest.fixture(scope="session")
 def config(pytestconfig):
     config_loc = pytestconfig.getoption("config")
     return get_config(config_loc)
 
+
 def get_config(config) -> EnvYAML:
     config = EnvYAML(config)
     return config
 
+
 @pytest.fixture
-def controller(start_component: list, config: str) -> RecommendationController:
+def controller(start_component: list, config: EnvYAML) -> RecommendationController:
     if not config.get(constants.MODEL_CONFIG_U2C):
         pytest.skip("configuration does not support u2c integration call")
     controller = RecommendationController(config)
     user_choice = mock_user_component(start_component)
     controller.model_type = constants.MODEL_TYPE_U2C
-    controller.register('user_choice', user_choice)
+    controller.register("user_choice", user_choice)
     return controller
 
+
 @pytest.fixture
-def u2c_controller(start_component: list, model: list, config: str) -> RecommendationController:
+def u2c_controller(
+    start_component: list, model: list, config: EnvYAML
+) -> RecommendationController:
     if not config.get(constants.MODEL_CONFIG_U2C):
         pytest.skip("configuration does not support u2c integration call")
     if not config[constants.MODEL_CONFIG_U2C][constants.MODEL_TYPE_U2C].get(model[0]):
-            pytest.skip("model with identifier [" + model[0] + "] not found in config - cant initialise. check test parameters!")
+        pytest.skip(
+            "model with identifier ["
+            + model[0]
+            + "] not found in config - cant initialise. check test parameters!"
+        )
     controller = RecommendationController(config)
     user_choice = mock_user_component(start_component)
-    controller.register('user_choice', user_choice)
-    controller.register('model_choice', mock_model_component(model))
+    controller.register("user_choice", user_choice)
+    controller.register("model_choice", mock_model_component(model))
     return controller
+
+
 @pytest.fixture
-def c2c_controller(selection_type: str, start_component: list, model: list, config: str) -> RecommendationController:
+def c2c_controller(
+    selection_type: str, start_component: list, model: list, config: EnvYAML
+) -> RecommendationController:
     if not config[constants.MODEL_CONFIG_C2C][constants.MODEL_TYPE_C2C].get(model[0]):
-            pytest.skip("model with identifier [" + model[0] + "] not found in config - cant initialise. check test parameters!")
+        pytest.skip(
+            "model with identifier ["
+            + model[0]
+            + "] not found in config - cant initialise. check test parameters!"
+        )
     controller = RecommendationController(config)
-    if selection_type == '_by_date':
-        for position in ['start', 'end']:
+    if selection_type == "_by_date":
+        for position in ["start", "end"]:
             date_choice = mock_date_component(start_component, position)
-            controller.register('item_choice', date_choice)
-    controller.register('model_choice', mock_model_component(model))
+            controller.register("item_choice", date_choice)
+    controller.register("model_choice", mock_model_component(model))
     return controller
+
