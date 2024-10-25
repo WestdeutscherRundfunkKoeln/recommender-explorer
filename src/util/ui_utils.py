d20ea00316e6800f5b9e14152d83cc30de7ea2f2
Bashir <m.bashir.qanaa@gmail.com>
2024-10-25 14:31:49 +0200

    This function is to get the default accordion state.

---
+++ src/util/ui_utils.py
@@ -0,0 +1,11 @@
+def retrieve_default_model_accordion(ui_config):
+    print("HI I'm here !!")
+
+    #loop through the ui_config file and look for the block Modelle wählen.
+    # in Modelle wählen identify the key that points to the active accordion and return it.
+    # if the block is called "Modelle wählen" then we should do the search procedure.
+
+
+
+
+        
