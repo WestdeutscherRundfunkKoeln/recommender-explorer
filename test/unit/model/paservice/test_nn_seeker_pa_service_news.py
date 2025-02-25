c32744e6289d77d6210fcd9e0fe0fbe77016ad5e
Michael Gruschke <michael.gruschke@pub.tech>
2025-02-25 15:44:20 +0100

    feat: add test

--- test/unit/model/paservice/test_nn_seeker_pa_service_news.py
+++ test/unit/model/paservice/test_nn_seeker_pa_service_news.py
@@ -1,9 +1,11 @@
 import json
+import pytest
 
 from urllib3 import HTTPResponse
 
 from src.dto.content_item import ContentItemDto
 from src.model.rest.nn_seeker_paservice_news import NnSeekerPaServiceNews
+from src.exceptions.item_not_found_error import UnknownItemError
 
 TEST_CONFIG = {
     "opensearch": {
@@ -57,6 +59,50 @@ def test_get_k_NN_WDR(mocker):
     assert ids == ["test2", "test1"]
     assert scores == [0.2, 0.1]
     assert oss_field == "externalid"
+    mock_request.assert_called_once_with(
+        "POST",
+        "https://test.io/recos",
+        json={
+            "referenceId": "test",
+            "excludedIds": ["test_blacklist_id1", "test_blacklist_id2"],
+            "maxDurationFactor": 100,
+            "reco": False,
+            "weights": [
+                {
+                    "type": "audio",
+                    "weight": 1,
+                },
+                {
+                    "type": "video",
+                    "weight": 2,
+                },
+            ],
+        },
+        headers={"test": "test"},
+    )
+
+
+def test_get_k_NN_exception(mocker):
+    mock_request = mocker.patch(
+        "urllib3.PoolManager.request",
+        return_value=HTTPResponse(
+            body=json.dumps({"error": "not found"}).encode(),
+            status=404,
+        ),
+    )
+    nn_seeker = NnSeekerPaServiceNews(TEST_CONFIG)
+    nn_seeker.set_model_config(TEST_MODEL_CONFIG_C2C)
+    with pytest.raises(Exception):
+        _ = nn_seeker.get_k_NN(
+            item=ContentItemDto("test", "test", "test", externalid="test"),
+            k=3,
+            nn_filter={
+                "relativerangefilter_duration": 100,
+                "blacklist_externalid": "test_blacklist_id1,test_blacklist_id2",
+                "weight_audio": 1,
+                "weight_video": 2,
+            },
+        )
     mock_request.assert_called_once_with(
         "POST",
         "https://test.io/recos",
