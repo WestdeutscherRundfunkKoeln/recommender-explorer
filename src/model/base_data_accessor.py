ea9ec24a35c634120b95981ec0e920a7f23ce0e7
Michael Gruschke <michael.gruschke@pub.tech>
2025-09-22 18:02:10 +0200

    chore: formatting

--- src/model/base_data_accessor.py
+++ src/model/base_data_accessor.py
@@ -6,14 +6,16 @@ from dto.item import ItemDto
 
 class BaseDataAccessor(ABC):
     @abstractmethod
-    def get_items_by_ids(self, item: ItemDto, ids: Collection[str]) -> list[ItemDto]:
-        pass
+    def get_items_by_ids(
+        self, item: ItemDto, ids: Collection[str]
+    ) -> list[ItemDto]: ...
 
     @abstractmethod
-    def get_primary_key_by_field(self, item_ident, field):
-        pass
+    def get_primary_key_by_field(self, item_ident, field): ...
 
     @abstractmethod
-    def get_unique_vals_for_column(self, column, sort=True):
-        pass
+    def get_unique_vals_for_column(self, column, sort=True): ...
+
+    @abstractmethod
+    def get_item_by_urn(self, item: ItemDto, urn: str) -> list[ItemDto]: ...
 
