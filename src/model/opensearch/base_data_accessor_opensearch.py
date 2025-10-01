86124a0e03944027f4bf19b6f6a2024b5b7ad7eb
Michael Gruschke <michael.gruschke@pub.tech>
2025-10-01 18:11:31 +0200

    fix: add oss methods back

--- src/model/opensearch/base_data_accessor_opensearch.py
+++ src/model/opensearch/base_data_accessor_opensearch.py
@@ -1,12 +1,19 @@
+import base64
+import collections
 import copy
 import logging
+import re
+from datetime import datetime
 from typing import Collection
 
+import pandas as pd
 from opensearchpy import OpenSearch, RequestsHttpConnection
-from model.base_data_accessor import BaseDataAccessor
-from exceptions.empty_search_error import EmptySearchError
+
+import constants
 from dto.item import ItemDto
-from util.dto_utils import update_from_props, get_primary_idents
+from exceptions.empty_search_error import EmptySearchError
+from model.base_data_accessor import BaseDataAccessor
+from util.dto_utils import get_primary_idents, update_from_props
 
 # loggin preference
 logger = logging.getLogger(__name__)
@@ -95,7 +102,7 @@ class BaseDataAccessorOpenSearch(BaseDataAccessor):
         response = self.client.search(body=query, index=self.target_idx_name)
         return self.__get_items_from_response(item, response)
 
-    def get_item_by_urn(self, item: ItemDto, urn: str):
+    def get_item_by_urn(self, item: ItemDto, urn, filter=None):
         urn = urn.strip()
         _, prim_val = get_primary_idents(self.config)
         return self._get_item_by_column_value(item=item, column=prim_val, value=urn)
@@ -154,3 +161,151 @@ class BaseDataAccessorOpenSearch(BaseDataAccessor):
             new_item = update_from_props(new_item, opensearch_hit, self.field_mapping)
             item_dtos.append(new_item)
         return item_dtos
+
+    def get_item_by_url(self, item: ItemDto, url, filter={}):
+        last_string = re.search(r".*/([^/?]+)[?]*", url.strip()).group(1)
+        base64_bytes = last_string.encode("ascii")
+        crid_bytes = base64.b64decode(base64_bytes + b"==")
+        crid = crid_bytes.decode("ascii")
+        return self.get_item_by_crid(item, crid, filter)
+
+    def _get_item_by_column_value(self, item: ItemDto, column: str, value: str):
+        oss_col = column + ".keyword"
+        query = {
+            "size": 10,  # duplicate crids max occur in data, return max 10
+            "_source": {"exclude": "embedding"},
+            "query": {
+                "match": {oss_col: value},
+            },
+        }
+        logger.info(query)
+        response = self.client.search(body=query, index=self.target_idx_name)
+        return self.__get_items_from_response(item, response)
+
+    def get_item_by_crid(self, item: ItemDto, crid, filter=None):
+        """Builds query to get items based on a crid
+
+        Maps crid parameter to mapped value if given in configuration file.
+        Creates and executes the query for opensearch service.
+
+        :param item: Item Dto from given component
+        :param crid: Value of crid from component
+        :param filter:
+        :return: Response from opensearch service for crid query
+        """
+        crid = crid.strip()
+        column = "crid"
+        # apply field mapping if defined
+        if column in self.field_mapping.keys():
+            new_col = self.field_mapping[column]
+            logger.info(f"mapping col {column} to {new_col}")
+            column = new_col
+        return self._get_item_by_column_value(item=item, column=column, value=crid)
+
+    def get_item_by_cms_id(self, item: ItemDto, cms_id: str, filter=None):
+        return self._get_item_by_column_value(item=item, column="cmsId", value=cms_id)
+
+    def get_item_by_text(self, item: ItemDto, text, filter={}):
+        item_dtos = []
+        new_item = copy.copy(item)
+        new_item._is_draft = True
+        text_input = {"description": text}
+        new_item = update_from_props(new_item, text_input, self.field_mapping)
+        item_dtos.append(new_item)
+        return item_dtos, 1
+
+    def get_items_by_date(
+        self,
+        item: ItemDto,
+        start_date: datetime,
+        end_date: datetime,
+        item_filter={},
+        offset=10,
+        size=-1,
+    ) -> tuple[list, int]:
+        # handle valid size range
+        if size < 0 or size > self.max_items_per_fetch:
+            size = self.max_items_per_fetch
+
+        if start_date > end_date:
+            logger.info(
+                f"end data for data selection. {end_date} is before start date {start_date}. Swapping end and start date."
+            )
+            xx = end_date
+            end_date = start_date
+            start_date = xx
+
+        query = self.__compose_date_range_query(
+            size, offset, [start_date, end_date], item_filter
+        )
+        logger.info(f"query: {query}")
+        response = self.client.search(body=query, index=self.target_idx_name)
+        return self.__get_items_from_response(item, response)
+
+    def get_items_date_range_limits(self) -> tuple[pd.Timestamp, pd.Timestamp]:
+        query = {
+            "size": 0,
+            "query": {"match_all": {}},
+            "aggs": {
+                "min_date": {"min": {"field": self.field_mapping["created"]}},
+                "max_date": {"max": {"field": self.field_mapping["created"]}},
+            },
+        }
+        logger.info(query)
+        response = self.client.search(body=query, index=self.target_idx_name)
+        min_date = response["aggregations"]["min_date"]["value_as_string"]
+        max_date = response["aggregations"]["max_date"]["value_as_string"]
+        newest_item_in_base_ts = pd.Timestamp(max_date).timestamp()
+        oldest_item_in_base_ts = pd.Timestamp(min_date).timestamp()
+        return newest_item_in_base_ts, oldest_item_in_base_ts
+
+    def __get_items_from_response(
+        self, item: ItemDto, response, provenance=constants.ITEM_PROVENANCE_C2C
+    ) -> tuple[list, int]:
+        """Gets the resulting items from the opensearch services response
+
+        Gets total items count from search response (hits.total.hits) and iterates
+        over result items (hits.hits._source)
+
+        :param item: Item dto from the given component
+        :param response: Response from opensearch service for created query
+        :param provenance:
+        :return: List of item dtos, total items count
+        """
+        total_items = response["hits"]["total"]["value"]
+        # items = [x["_source"] for x in response["hits"]["hits"]]
+        items = []
+        for x in response["hits"]["hits"]:
+            if "_source" in x:
+                items.append(x["_source"])
+
+        if total_items < 1 or not len(items):
+            raise EmptySearchError("Keine Treffer gefunden", {})
+        item_dtos = []
+        for opensearch_hit in items:
+            new_item = copy.copy(item)
+            new_item = update_from_props(new_item, opensearch_hit, self.field_mapping)
+            item_dtos.append(new_item)
+        return item_dtos, total_items
+
+    def __compose_date_range_query(self, size, offset, dates, item_filter) -> dict:
+        query = {
+            "size": size,
+            "from": offset,
+            "_source": {"exclude": "embedding*"},
+            "query": {
+                "bool": {
+                    "must": [
+                        {"range": {"availableFrom": {"gte": dates[0], "lt": dates[1]}}},
+                    ]
+                }
+            },
+            "sort": [{self.field_mapping["created"]: {"order": "asc"}}],
+        }
+
+        for filter_category in item_filter.items():
+            term = collections.defaultdict(dict)
+            if len(filter_category[1]) > 0:
+                term["terms"][filter_category[0] + ".keyword"] = filter_category[1]
+                query["query"]["bool"]["must"].append(term)
+        return query
